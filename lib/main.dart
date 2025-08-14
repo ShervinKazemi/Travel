7d96a6f387f5dce74b91f3dea90a6c17c8767a8e
Shervin <github.kazmy2@gmail.com>
2025-08-14 19:01:08 +0330

    feat(develop): add theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:travel/app.dart';
 
 void main() {
+
+  final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark
+    )
+  );
+  FlutterNativeSplash.remove();
+
   runApp(
     const App()
   );
