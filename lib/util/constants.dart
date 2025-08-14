5bf568d3f2800af23e78976e82890e5bdd6eb045
Shervin <github.kazmy2@gmail.com>
2025-08-14 18:34:19 +0330

    feat(develop): add constants.dart

---
+++ lib/util/constants.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static const String travelsData = "assets/data/travels.json";
+}
+
+class AppColors {
+  static const Color primary = Color(0x001b1b1b);
+  // Light Theme Colors
+  static const Color lightText = Colors.black;
+  static const Color lightHint = Colors.grey;
+  static const Color lightInputFill = Color(0xFFF5F5F5);
+  static const Color lightBorder = Colors.grey;
+  static const Color lightFocusedBorder = Colors.black;
+}
