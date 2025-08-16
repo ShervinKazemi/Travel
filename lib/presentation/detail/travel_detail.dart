692d86519a5eb8cbe355dbf867472b2275e4d3ce
Shervin <github.kazmy2@gmail.com>
2025-08-16 14:40:12 +0330

    feat(develop): add travel_detail.dart

--- lib/presentation/detail/travel_detail.dart
+++ lib/presentation/detail/travel_detail.dart
@@ -16,144 +16,186 @@ class TravelDetail extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: Stack(
             children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(32),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: MediaQuery.of(context).size.height * 0.5,
-                  child: Stack(
-                    fit: StackFit.expand,
-                    children: [
-                      Hero(
-                        tag: "travel${data.name}",
-                        child: Image.asset(data.image, fit: BoxFit.cover),
-                      ),
-                      Positioned(
-                        top: 16,
-                        right: 16,
-                        left: 16,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Container(
-                              width: 50,
-                              height: 50,
-                              decoration: BoxDecoration(
-                                color: Colors.grey.shade700,
-                                borderRadius: BorderRadius.circular(52),
-                              ),
-                              child: IconButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                icon: Icon(
-                                  Icons.keyboard_arrow_left_rounded,
-                                  color: Colors.white,
-                                  size: 24,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 50,
-                              height: 50,
-                              decoration: BoxDecoration(
-                                color: Colors.grey.shade700,
-                                borderRadius: BorderRadius.circular(52),
-                              ),
-                              child: Icon(
-                                CupertinoIcons.bookmark,
-                                color: Colors.white,
-                                size: 24,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Positioned(
-                        bottom: 16,
-                        right: 16,
-                        left: 16,
-                        child: GlassMorphism(
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 16,
-                              vertical: 8,
-                            ),
-                            child: Column(
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(32),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: MediaQuery.of(context).size.height * 0.5,
+                      child: Stack(
+                        fit: StackFit.expand,
+                        children: [
+                          Hero(
+                            tag: "travel${data.name}",
+                            child: Image.asset(data.image, fit: BoxFit.cover),
+                          ),
+                          Positioned(
+                            top: 16,
+                            right: 16,
+                            left: 16,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      data.name,
-                                      style: textTheme.headlineMedium!.copyWith(
-                                        color: Colors.white,
-                                      ),
+                                Container(
+                                  width: 50,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey.shade700,
+                                    borderRadius: BorderRadius.circular(52),
+                                  ),
+                                  child: IconButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    icon: Icon(
+                                      Icons.keyboard_arrow_left_rounded,
+                                      color: Colors.white,
+                                      size: 24,
                                     ),
-                                    Text(
-                                      "Price",
-                                      style: textTheme.displayMedium!.copyWith(
-                                        color: Colors.grey.shade300,
-                                      ),
-                                    ),
-                                  ],
+                                  ),
                                 ),
-                                Gap(8),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        Icon(
-                                          Icons.location_on_outlined,
-                                          color: Colors.grey.shade300,
-                                          size: 16,
-                                        ),
-                                        Text(
-                                          "${data.city}, ${data.country}",
-                                          style: textTheme.displayMedium!.copyWith(
-                                            color: Colors.grey.shade300
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                    Text(
-                                      "\$ ${data.price}",
-                                      style: textTheme.displayMedium!.copyWith(
-                                        color: Colors.grey.shade300
-                                      ),
-                                    ),
-                                  ],
+                                Container(
+                                  width: 50,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey.shade700,
+                                    borderRadius: BorderRadius.circular(52),
+                                  ),
+                                  child: Icon(
+                                    CupertinoIcons.bookmark,
+                                    color: Colors.white,
+                                    size: 24,
+                                  ),
                                 ),
                               ],
                             ),
                           ),
-                        ),
+                          Positioned(
+                            bottom: 16,
+                            right: 16,
+                            left: 16,
+                            child: GlassMorphism(
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 16,
+                                  vertical: 8,
+                                ),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          data.name,
+                                          style: textTheme.headlineMedium!
+                                              .copyWith(color: Colors.white),
+                                        ),
+                                        Text(
+                                          "Price",
+                                          style: textTheme.displayMedium!
+                                              .copyWith(
+                                                color: Colors.grey.shade300,
+                                              ),
+                                        ),
+                                      ],
+                                    ),
+                                    Gap(8),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Icon(
+                                              Icons.location_on_outlined,
+                                              color: Colors.grey.shade300,
+                                              size: 16,
+                                            ),
+                                            Text(
+                                              "${data.city}, ${data.country}",
+                                              style: textTheme.displayMedium!
+                                                  .copyWith(
+                                                    color: Colors.grey.shade300,
+                                                  ),
+                                            ),
+                                          ],
+                                        ),
+                                        Text(
+                                          "\$ ${data.price}",
+                                          style: textTheme.displayMedium!
+                                              .copyWith(
+                                                color: Colors.grey.shade300,
+                                              ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
+                    ),
+                  ),
+                  Gap(16),
+                  Text(
+                    "Overview",
+                    style: textTheme.headlineLarge,
+                    textDirection: TextDirection.ltr,
+                    textAlign: TextAlign.start,
+                  ),
+                  Gap(16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      OptionWidget(
+                        text: data.time,
+                        icon: Icons.timelapse_rounded,
+                      ),
+                      OptionWidget(text: data.tempreture, icon: Icons.cloud),
+                      OptionWidget(text: data.rating, icon: Icons.star_rounded),
+                    ],
+                  ),
+                  Gap(16),
+                  Text(
+                    data.description,
+                    textAlign: TextAlign.justify,
+                    textDirection: TextDirection.ltr,
+                    style: textTheme.bodyLarge,
+                  )
+                ],
+              ),
+              Positioned(
+                bottom: 16,
+                right: 0,
+                left: 0,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 32 , vertical: 16),
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                  ),
+                  onPressed: () {
+                    
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Book Now" , style: textTheme.headlineMedium!.copyWith(color: Colors.white)),
+                      Gap(16),
+                      Icon(
+                        Icons.near_me_rounded,
+                        size: 24,
+                      )
                     ],
                   ),
                 ),
-              ),
-              Gap(16),
-              Text(
-                "Overview",
-                style: textTheme.headlineLarge,
-                textDirection: TextDirection.ltr,
-                textAlign: TextAlign.start,
-              ),
-              Gap(16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  OptionWidget(text: data.time, icon: Icons.timelapse_rounded),
-                  OptionWidget(text: data.tempreture, icon: Icons.cloud),
-                  OptionWidget(text: data.rating, icon: Icons.star_rounded)
-                ],
               )
             ],
           ),
